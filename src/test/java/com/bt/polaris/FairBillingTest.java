d6f5a7c3bbaea5b040cdacfd01fbf3e0040b9b57
Keiko Morimoto <keiko.morimoto@bt.com>
2023-06-22 20:24:27 +0100

    test resources added

--- src/test/java/com/bt/polaris/FairBillingTest.java
+++ src/test/java/com/bt/polaris/FairBillingTest.java
@@ -8,10 +8,29 @@ import static com.bt.polaris.FairBilling.processLogFile;
 class FairBillingTest {
     @Test
     public void processLogFileTest(){
-        String actualResult = processLogFile("C:\\Users\\615111023\\Code\\fair-billing\\logfile2.txt");
-        String Expected = "ALICE99 4 240\n" +
-                "CHARLIE 4 38\n";
+        String actualResult = processLogFile("C:\\Users\\615111023\\Code\\fair-billing\\src\\test\\resources\\logfile.txt");
+        String Expected = "ALICE99 4 240\r\n" +
+                "CHARLIE 3 37\r\n";
+        assertEquals(Expected,actualResult);
+    }
+    @Test
+    public void processLogFileTestWithInvalidInput(){
+        String actualResult = processLogFile("C:\\Users\\615111023\\Code\\fair-billing\\src\\test\\resources\\logfile2.txt");
+        String Expected = "ALICE99 4 240\r\n" +
+                "CHARLIE 4 38\r\n";
         assertEquals(Expected,actualResult);
     }
 
+    @Test
+    public void processLogFileTestWithStartOnly(){
+        String actualResult = processLogFile("C:\\Users\\615111023\\Code\\fair-billing\\src\\test\\resources\\logfile3.txt");
+        String Expected = "ALICE99 1 0\r\n";
+        assertEquals(Expected,actualResult);
+    }
+    @Test
+    public void processLogFileTestWithTwoExtraEnd(){
+        String actualResult = processLogFile("C:\\Users\\615111023\\Code\\fair-billing\\src\\test\\resources\\logfile4.txt");
+        String Expected = "ALICE99 3 124\r\n";
+        assertEquals(Expected,actualResult);
+    }
 }
