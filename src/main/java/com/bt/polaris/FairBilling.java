044a6965846b019e428dd848134a74c4a5be70be
Keiko Morimoto <keiko.morimoto@bt.com>
2023-07-06 09:52:39 +0100

    created SessionData class

--- src/main/java/com/bt/polaris/FairBilling.java
+++ src/main/java/com/bt/polaris/FairBilling.java
@@ -3,12 +3,8 @@ package com.bt.polaris;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
 
 public class FairBilling {
 
@@ -23,76 +19,50 @@ public class FairBilling {
         System.out.println(result);
     }
 
-
     public static String processLogFile(String logFilePath) {
-        String result = "";
-
-        Map<String, Integer> sessionCounts = new HashMap<>();
-        Map<String, Long> sessionDurations = new HashMap<>();
-        Map<String, Stack<LocalTime>> sessionStacks = new HashMap<>();
-
         LocalTime earliestTime = null;
         LocalTime latestTime = null;
+        SessionData SessionData = new SessionData();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(logFilePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
-                if (!line.isEmpty()) {
-                    String[] record = line.split(" ");
-                    if (record.length == 3) {
-                        String timeStr = record[0];
-                        String username = record[1];
-                        String status = record[2];
 
-                        try {
-                            LocalTime time = LocalTime.parse(timeStr, DateTimeFormatter.ofPattern("HH:mm:ss"));
-
-                            if (earliestTime == null) earliestTime = time;
-                            if (latestTime == null || time.isAfter(latestTime)) latestTime = time;
-
-                            if (status.equals("Start")) {
-                                sessionCounts.put(username, sessionCounts.getOrDefault(username, 0) + 1);
-                                sessionStacks.computeIfAbsent(username, k -> new Stack<>()).push(time);
-
-                            } else if (status.equals("End")) {
-                                Stack<LocalTime> stack = sessionStacks.get(username);
-                                if (stack != null && !stack.isEmpty()) {
-                                    long duration = sessionDurations.getOrDefault(username, 0L) + Duration.between(stack.pop(), time).getSeconds();
-                                    sessionDurations.put(username, duration);
-                                }
-                                else{  // if there is END with no matching Start
-                                    sessionCounts.put(username, sessionCounts.getOrDefault(username, 0) + 1);
-                                    long duration = sessionDurations.getOrDefault(username, 0L) + Duration.between(earliestTime, time).getSeconds();
-                                    sessionDurations.put(username, duration);
-                                }
-                            }
-                        } catch (Exception e) {
-                            System.out.println(" It was not read correctly: " + e);
-                        }
-                    }
+                if (line.isEmpty()) {
+                    continue;
                 }
+
+                String[] record = line.split(" ");
+                if (record.length != 3) {
+                    continue;
+                }
+
+                String timeStr = record[0];
+                String username = record[1];
+                String status = record[2];
+
+                try {
+                    LocalTime time = LocalTime.parse(timeStr, DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+                    if (earliestTime == null) earliestTime = time;
+                    if (latestTime == null || time.isAfter(latestTime)) latestTime = time;
+
+                    if (status.equals("Start")) {
+                        SessionData.addStart(username, time);
+                    } else if (status.equals("End")) {
+                        SessionData.addEnd(username, time, earliestTime);
+                    }
+                } catch (Exception e) {}
             }
         } catch (IOException e) {
             System.out.println("Error reading the log file: " + e.getMessage());
         }
 
         // When there were START with no matching END
-        for (String username : sessionStacks.keySet()) {
-            Stack<LocalTime> stack = sessionStacks.get(username);
-            while (!stack.isEmpty()) {
-                long duration = sessionDurations.getOrDefault(username, 0L) + Duration.between(stack.pop(), latestTime).getSeconds();
-                sessionDurations.put(username, duration);
-            }
-        }
+        SessionData.addExtraStart(latestTime);
 
-        // Prepare the string message to display
-        for (String username : sessionCounts.keySet()) {
-            int sessionCount = sessionCounts.get(username);
-            long sessionDuration = sessionDurations.getOrDefault(username, 0L);
-            result += String.format("%s %d %d%n", username, sessionCount, sessionDuration);
-        }
-
-        return result;
+        return SessionData.formatResult();
     }
 }
+
